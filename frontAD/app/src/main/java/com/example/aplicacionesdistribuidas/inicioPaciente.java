25f52cd4da101219c3ab2ce99efa1cb8daabf52b
Lucila Masini <lulym2008@hotmail.com>
2020-04-30 20:26:48 -0300

    cambio tipodeletra y agrego iniciopaciente
    cambie la letra a roboto_light y cree el inicio del paciente(por ahora esta vacio)

---
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/inicioPaciente.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionesdistribuidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicioPaciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_paciente);
+    }
+}
