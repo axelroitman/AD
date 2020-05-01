6974e5b1589c15a9eba28e55b3c7000e6e5d8f5b
diegoritu <49616722+diegoritu@users.noreply.github.com>
2020-05-01 02:12:11 -0300

    Inicio Switch Paciente/Médico
    Nuevo activity para los médicos que sean pacientes también, para poder switchear entre sus distintos roles. La lógica a seguir sería que si quiere ver lo que puede hacer como médico toca el estetoscopio de arriba a la derecha, y si quiere ver lo que puede hacer como paciente, toca al personaje con un yeso. Esto alternaría las figuras, marcando la seleccionada con color blanco, y la secundaria con negro, rotando sus posiciones en el triángulo también. Se me ocurre que en un tiempo esa rotación quizás puede ser una animación también.

---
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/inicioMedPac_Medico.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionesdistribuidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicioMedPac_Medico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_med_pac_Medico);
+    }
+}
