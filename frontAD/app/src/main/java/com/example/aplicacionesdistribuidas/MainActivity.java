f5b5ecab6abe3f96424654f93e580d7146e151e1
axelroitman <38115641+axelroitman@users.noreply.github.com>
2020-05-01 15:32:07 -0300

    Nuevo indicador de turno
    Nuevo indicador próximo turno en el inicio de usuario.

--- frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/MainActivity.java
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.aplicacionesdistribuidas;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -15,12 +17,28 @@ public class MainActivity extends AppCompatActivity {
     EditText txtusuario, txtcontrasena;
     TextView tvregistrarse;
     Button btningresar;
-
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Error");
+        builder.setMessage("Usuario y/o contraseña incorrectos.");
+        builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.show();
+
         vincular();
 
 
