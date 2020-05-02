560489f2ae246762ad7bf68aeef55a1dcb036f97
Lucila Masini <lulym2008@hotmail.com>
2020-05-02 17:33:45 -0300

    Update Turno.java
    para que aparezca el mensaje de error cuando se elimina un turno

--- frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/Turno.java
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/Turno.java
@@ -1,14 +1,34 @@
 package com.example.aplicacionesdistribuidas;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class Turno extends AppCompatActivity {
+
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turno);
+
+
+        builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Atención");
+        builder.setMessage("El turno ha sido eliminado.");
+        builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.show();
     }
 }
