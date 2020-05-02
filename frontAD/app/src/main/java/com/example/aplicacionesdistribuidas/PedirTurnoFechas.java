76ec5ced74649097ac60db38108534d85902c195
diegoritu <49616722+diegoritu@users.noreply.github.com>
2020-05-02 20:41:10 -0300

    Pedir turno fechas

---
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/PedirTurnoFechas.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionesdistribuidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PedirTurnoFechas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedir_turno_fechas);
+    }
+}
