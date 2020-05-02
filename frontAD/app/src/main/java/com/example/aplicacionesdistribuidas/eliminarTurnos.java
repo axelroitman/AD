e194651dacbb08b3fc0e807066c1d031b5ee890a
axelroitman <38115641+axelroitman@users.noreply.github.com>
2020-05-02 19:09:06 -0300

    Eliminar turnos y cambios en agenda médico

---
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/eliminarTurnos.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionesdistribuidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eliminarTurnos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_turnos);
+    }
+}
