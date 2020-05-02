de7e98b39576b5cf7537cd8027afe0aa6e2abd0d
diegoritu <49616722+diegoritu@users.noreply.github.com>
2020-05-02 13:44:53 -0300

    Historial turnos y JustifInasistencia (en 0)
    Co-Authored-By: lulamasini <lulamasini@users.noreply.github.com>

---
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/JustificacionInasistencia.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionesdistribuidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JustificacionInasistencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_justificacion_inasistencia);
+    }
+}
---
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/historial_turnos.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionesdistribuidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class historial_turnos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_turnos);
+    }
+}
