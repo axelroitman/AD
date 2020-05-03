d3837f03aa87b88ac2236881895751e78db58fe5
Lucila Masini <lulym2008@hotmail.com>
2020-05-02 21:57:41 -0300

    VistaModificarTurno

---
+++ frontAD/app/src/main/java/com/example/aplicacionesdistribuidas/modificar_turno.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionesdistribuidas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modificar_turno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_turno);
+    }
+}
