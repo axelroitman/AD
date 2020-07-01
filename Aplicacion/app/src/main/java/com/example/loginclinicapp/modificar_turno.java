63ce0c305c37e1318416a9d9252dce3be59b576f
diegoritu <49616722+diegoritu@users.noreply.github.com>
2020-07-01 00:15:13 -0300

    Avances en modificar turno

--- Aplicacion/app/src/main/java/com/example/loginclinicapp/modificar_turno.java
+++ Aplicacion/app/src/main/java/com/example/loginclinicapp/modificar_turno.java
@@ -3,9 +3,15 @@ package com.example.loginclinicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class modificar_turno extends AppCompatActivity {
 
+    ImageView imgEspecialidad, imgEstado;
+    TextView estado, especialidad, precio;
+    TextView hora;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
